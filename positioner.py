f0bd06f4ec71dda94c514c9e9b40916fc77198b5
chiora93 <chiora93@gmail.com>
2016-05-29 13:25:16 +0200

    Run the script only if is invoked from the terminal

--- positioner.py
+++ positioner.py
@@ -1,37 +1,39 @@
 import os
 import shutil
 
-from settings import DOWNLOAD_DIR, dst_dir1,dst_dir2
+from settings import DOWNLOAD_DIR, dst_dir1, dst_dir2
 
-download_list = os.listdir(DOWNLOAD_DIR)
+if __name__ == '__main__':
 
-pairs = []
+    download_list = os.listdir(DOWNLOAD_DIR)
 
-for file in download_list:
+    pairs = []
 
-    # Se e' una directory fai questo questo e questo
+    for file in download_list:
 
-    location = os.path.join(DOWNLOAD_DIR, file)
-    if os.path.isdir(location):
-        def get_size():
-            total_size = 0
-            for dirpath, dirnames, filenames in os.walk(location):
-                for f in filenames:
-                    fp = os.path.join(dirpath, f)
-                    total_size += os.path.getsize(fp)
-            return total_size
+        # Se e' una directory fai questo questo e questo
+
+        location = os.path.join(DOWNLOAD_DIR, file)
+        if os.path.isdir(location):
+            def get_size():
+                total_size = 0
+                for dirpath, dirnames, filenames in os.walk(location):
+                    for f in filenames:
+                        fp = os.path.join(dirpath, f)
+                        total_size += os.path.getsize(fp)
+                return total_size
 
 
-        if get_size() > 700000000L:
-            for file in os.listdir(os.path.join(location)):
-                if file.endswith((".mkv", ".avi", "mp4")):  # Sara' un film o un pornazzo
-                    shutil.move(os.path.join(location), dst_dir1)
+            if get_size() > 700000000L:
+                for file in os.listdir(os.path.join(location)):
+                    if file.endswith((".mkv", ".avi", "mp4")):  # Sara' un film o un pornazzo
+                        shutil.move(os.path.join(location), dst_dir1)
+            else:
+                shutil.move(os.path.join(location), dst_dir2)
+
+        # Altrimenti se e' un cazzo di file fai questo questo e questo
+
+        elif file.endswith((".mkv", ".avi")):
+            shutil.move(os.path.join(location), dst_dir1)
         else:
             shutil.move(os.path.join(location), dst_dir2)
-
-    # Altrimenti se e' un cazzo di file fai questo questo e questo
-
-    elif file.endswith((".mkv", ".avi")):
-        shutil.move(os.path.join(location), dst_dir1)
-    else:
-        shutil.move(os.path.join(location), dst_dir2)
